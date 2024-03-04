d29c0cd4cc3ce945878a86fcc6988b801b8a150e
konrad101296 <konradslimak@o2.pl>
2024-03-04 19:52:54 +0100

    Add project files.

---
+++ week 2 lesson 8 exercise nr.6/Program.cs
@@ -0,0 +1,9 @@
+﻿double numerator = 1;
+double fraction;
+double sum = 0;
+for (int i = 1; i <= 20; i++)
+{
+    fraction = numerator / i;
+    sum += fraction;
+}
+Console.WriteLine($"result {sum}");
